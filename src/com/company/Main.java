636678a43e3346ab52b3860884c9f7aec214c5ad
sakorepanov <sakorepanov@mail.ru>
2020-07-20 22:12:17 +0400

    not known

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("First string");
+        System.out.println("First string, first");
     }
 }
